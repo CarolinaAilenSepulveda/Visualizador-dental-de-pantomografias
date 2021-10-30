3730e67662f1f1b5f75d4e1f846edde50319aadd
Carolina Sepulveda <carosepulveda99@gmail.com>
2021-10-30 19:15:15 -0300

    Se posiciona la panorámica sobre el cilindro, el movimiento es automático

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,9 @@
-import React, {useRef} from "react"
+import React, {useRef, Suspense} from "react"
 import {Canvas, useFrame} from "@react-three/fiber"
 import {useTexture, Loader} from "@react-three/drei"
 
-import imagen from "../assets/menos.png"
+//Importo imagen de la carpeta assets, solo cambio el nombre para elegir una panorámica
+import imagen from "../assets/foto_1.png"
 
 //Armado del cilindro
 function Cilindro(props) {
@@ -10,14 +11,14 @@ function Cilindro(props) {
   const texture = useTexture(imagen)
 
   useFrame(() => {
-    ref.current.rotation.x = ref.current.rotation.y //Rotación respecto de x e y
+    //Para que solo rote en un sentido, para más rapidez aumentar 0.01
+    ref.current.rotation.y += 0.004
   })
 
   //Escala 1:1, args(radio sup,radio inf, altura, vértices, número de caras altura)
-  //Color rosa
   return (
     <mesh {...props} ref={ref} scale={1}>
-      <cylinderGeometry args={[1, 1, 2, 100, 1]} />
+      <cylinderGeometry args={[2, 2, 4, 100, 1]} />
       <meshPhysicalMaterial map={texture} />
     </mesh>
   )
@@ -25,18 +26,21 @@ function Cilindro(props) {
 
 //Armado escena
 //Acomodo color de fondo, intensidad, angulo y posición de la luz estándar, posición estándar del cilindro
+//Centro el cilindro en el medio de la escena
+
 export default function App() {
   return (
     <div style={{width: "100%", backgroundColor: "lightblue", height: "100vh"}}>
       <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight angle={0.15} penumbra={1} position={[10, 10, 10]} />
-        <pointLight position={[-10, -10, -10]} />
-        <Cilindro position={[-1.2, 0, 0]} />
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.5} />
+          <spotLight angle={0.15} penumbra={1} position={[10, 10, 10]} />
+          <pointLight position={[-10, -10, -10]} />
+
+          <Cilindro position={[0, 0, 0]} />
+        </Suspense>
       </Canvas>
       <Loader />
     </div>
   )
 }
-
-//Para ejecutar Yarn run start
