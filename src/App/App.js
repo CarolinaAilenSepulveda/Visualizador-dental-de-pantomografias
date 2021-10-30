ee33f91bed46ddc971e987aebab8646e911b21c5
Carolina Sepulveda <carosepulveda99@gmail.com>
2021-10-30 17:53:29 -0300

    Agregue imagenes como textura

--- src/App/App.js
+++ src/App/App.js
@@ -1,30 +1,42 @@
 import React, {useRef} from "react"
 import {Canvas, useFrame} from "@react-three/fiber"
+import {useTexture, Loader} from "@react-three/drei"
 
-function Box(props) {
+import imagen from "../assets/jirafa.png"
+
+//Armado del cilindro
+function Cilindro(props) {
   const ref = useRef()
+  const texture = useTexture(imagen)
 
   useFrame(() => {
-    ref.current.rotation.x = ref.current.rotation.y
+    ref.current.rotation.x = ref.current.rotation.y //Rotación respecto de x e y
   })
 
+  //Escala 1:1, args(radio sup,radio inf, altura, vértices, número de caras altura)
+  //Color rosa
   return (
     <mesh {...props} ref={ref} scale={1}>
       <cylinderGeometry args={[1, 1, 2, 100, 1]} />
-      <meshStandardMaterial color={"hotpink"} />
+      <meshPhysicalMaterial map={texture} />
     </mesh>
   )
 }
 
+//Armado escena
+//Acomodo color de fondo, intensidad, angulo y posición de la luz estándar, posición estándar del cilindro
 export default function App() {
   return (
     <div style={{width: "100%", backgroundColor: "lightblue", height: "100vh"}}>
       <Canvas>
-        <ambientLight intensity={1} />
+        <ambientLight intensity={0.5} />
         <spotLight angle={0.15} penumbra={1} position={[10, 10, 10]} />
         <pointLight position={[-10, -10, -10]} />
-        <Box position={[-1.2, 0, 0]} />
+        <Cilindro position={[-1.2, 0, 0]} />
       </Canvas>
+      <Loader />
     </div>
   )
 }
+
+//Para ejecutar Yarn run start
